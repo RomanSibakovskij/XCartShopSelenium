322ed3f51283e5c57e03f699450fe973c873ba82
RomanSibakovskij <benutzer373@gmail.com>
2025-04-30 17:40:56 +0300

    Add empty HomePage sheet

---
+++ src/main/java/com/xcart/demo/HomePage.java
@@ -0,0 +1,19 @@
+package com.xcart.demo;
+
+import com.xcart.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class HomePage extends BasePage{
+
+
+    public HomePage(WebDriver driver) {super(driver);}
+
+    
+}
