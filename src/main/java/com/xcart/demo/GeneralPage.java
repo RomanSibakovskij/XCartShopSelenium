468ac5be924d05bd3238c4061dc2aa9bd5df6986
RomanSibakovskij <benutzer373@gmail.com>
2025-04-30 17:32:25 +0300

    Add empty GeneralPage sheet

---
+++ src/main/java/com/xcart/demo/GeneralPage.java
@@ -0,0 +1,20 @@
+package com.xcart.demo;
+
+import com.xcart.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.util.*;
+import java.time.Duration;
+
+public class GeneralPage extends BasePage {
+
+
+    public GeneralPage(WebDriver driver) {super(driver);}
+
+
+    
+}
+
