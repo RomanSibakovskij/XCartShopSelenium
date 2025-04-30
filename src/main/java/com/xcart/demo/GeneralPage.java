a56d75c957a9a3f479d464a848017a12dc563d0e
RomanSibakovskij <benutzer373@gmail.com>
2025-04-30 17:39:19 +0300

    Add web elements for GeneralPage

--- src/main/java/com/xcart/demo/GeneralPage.java
+++ src/main/java/com/xcart/demo/GeneralPage.java
@@ -11,10 +11,240 @@ import java.time.Duration;
 
 public class GeneralPage extends BasePage {
 
+    //general page web elements
+    //header (switcher)
+    @FindBy(xpath = "//div[@class='demo-box']//img")
+    private WebElement headerSwitcherIcon;
+    @FindBy(xpath = "//div[@class='demo-box']/div[2]/span")
+    private WebElement headerInterfaceTag;
+    @FindBy(xpath = "//div[@class='demo-box']/div[2]/select")
+    private WebElement headerInterfaceDropdownMenu;
+    @FindBy(xpath = "//div[@class='demo-box']/div[3]/span")
+    private WebElement headerVisualSchemeTag;
+    @FindBy(xpath = "//div[@class='demo-box']/div[3]/select")
+    private WebElement headerVisualSchemeDropdownMenu;
+    @FindBy(xpath = "//div[@class='demo-box']/div[4]/span")
+    private WebElement headerConfigurationTag;
+    @FindBy(xpath = "//div[@class='demo-box']/div[4]/select")
+    private WebElement headerConfigurationDropdownMenu;
+    @FindBy(xpath = "//div[@class='demo-box']/div[5]/span")
+    private WebElement headerQuickAddonsPanelTag;
+    @FindBy(xpath = "//div[@class='demo-box']/div[5]/a")
+    private WebElement headerQuickAddonsPanelSlideButton;
+    @FindBy(xpath = "//div[@class='demo-button']/a")
+    private WebElement headerSwitcherDownloadButton;
+    //header
+    @FindBy(xpath = "//div[@id='header']//div[@class='logo']/a")
+    private WebElement headerHomepageLogo;
+    @FindBy(xpath = "//div[@class='header-links']/a[1]")
+    private WebElement headerSignInLink;
+    @FindBy(xpath = "//div[@class='header-links']/a[2]")
+    private WebElement headerRegisterLink;
+    @FindBy(xpath = "//div[@class='languages languages-flags']/ul")
+    private WebElement headerLanguageDropdownMenu;
+    @FindBy(xpath = "//div[@class='search']//input[@name='posted_data[substring]']")
+    private WebElement headerSearchBarInputField;
+    @FindBy(xpath = "//div[@class='search']//button[@class='search-button']")
+    private WebElement headerSearchBarButton;
+    @FindBy(xpath = "//div[@class='menu-dialog menu-minicart']")
+    private WebElement headerShoppingCartDropdownButton;
+    //navbar
+    @FindBy(xpath = "//div[@class='navbar']/ul/li[1]/a")
+    private WebElement navbarHomeLink;
+    @FindBy(xpath = "//div[@class='navbar']/ul/li[2]/a")
+    private WebElement navbarShoppingCartLink;
+    @FindBy(xpath = "//div[@class='navbar']/ul/li[3]/a")
+    private WebElement navbarContactUsLink;
+    //aside elements (not all pages have them)
+    //categories
+    @FindBy(xpath = "//div[@class='menu-dialog menu-fancy-categories-list']//h2")
+    private WebElement asideCategoriesSectionTitle;
+    @FindBy(xpath = "//div[@id='catrootmenu']/ul/li[1]/a") //for register page //div[@id='catrootmenu']/div/ul/li[1]/a (added div element)
+    private WebElement asideFeaturesDemoLink;
+    @FindBy(xpath = "//div[@id='catrootmenu']/ul/li[2]/a")
+    private WebElement asideApparelLink;
+    @FindBy(xpath = "//div[@id='catrootmenu']/ul/li[3]/a")
+    private WebElement asideToysLink;
+    @FindBy(xpath = "//div[@id='catrootmenu']/ul/li[4]/a")
+    private WebElement asideIGoodsLink;
+    @FindBy(xpath = "//div[@id='catrootmenu']/ul/li[5]/a")
+    private WebElement asideBooksLink;
+    @FindBy(xpath = "//div[@id='catrootmenu']/ul/li[6]/a")
+    private WebElement asideComputerHardwareLink;
+    //other pages have included div between the id'ed div and ul (locator differences)
+    @FindBy(xpath = "//div[@id='catrootmenu']/div/ul/li[1]/a") //for register page //div[@id='catrootmenu']/div/ul/li[1]/a (added div element)
+    private WebElement asideOtherFeaturesDemoLink;
+    @FindBy(xpath = "//div[@id='catrootmenu']/div/ul/li[2]/a")
+    private WebElement asideOtherApparelLink;
+    @FindBy(xpath = "//div[@id='catrootmenu']/div/ul/li[3]/a")
+    private WebElement asideOtherToysLink;
+    @FindBy(xpath = "//div[@id='catrootmenu']/div/ul/li[4]/a")
+    private WebElement asideOtherIGoodsLink;
+    @FindBy(xpath = "//div[@id='catrootmenu']/div/ul/li[5]/a")
+    private WebElement asideOtherBooksLink;
+    @FindBy(xpath = "//div[@id='catrootmenu']/div/ul/li[6]/a")
+    private WebElement asideOtherComputerHardwareLink;
+    //bestsellers (not all pages have them)
+    @FindBy(xpath = "//div[@class='menu-dialog menu-products menu-products-thumbnails menu-bestsellers']//h2")
+    private WebElement asideBestsellersSectionTitle;
+    //list elements
+    @FindBy(xpath = "//div[@class='menu-dialog menu-products menu-products-thumbnails menu-bestsellers']//img")
+    private List<WebElement> asideBestProductImageElements;
+    @FindBy(xpath = "//div[@class='menu-dialog menu-products menu-products-thumbnails menu-bestsellers']//a[@class='product-title']")
+    private List<WebElement> asideBestProductNameLinkElements;
+    @FindBy(xpath = "//div[@class='menu-dialog menu-products menu-products-thumbnails menu-bestsellers']//span[@class='price-value']")
+    private List<WebElement> asideBestProductUnitPriceElements;
+    //manufacturers
+    @FindBy(xpath = "//div[@class='menu-dialog menu-manufacturers']//h2")
+    private WebElement asideManufacturersSectionTitle;
+    //list elements
+    @FindBy(xpath = "//div[@class='menu-dialog menu-manufacturers']//a")
+    private List<WebElement> asideManufacturerLinkElements;
+    //gift certificate
+    @FindBy(xpath = "//div[@class='gift-certificate']/a")
+    private WebElement asideGiftCertificateButton;
+    //special
+    @FindBy(xpath = "//div[@class='menu-dialog menu-special']//h2")
+    private WebElement asideSpecialSectionTitle;
+    @FindBy(xpath = "//div[@class='menu-dialog menu-special']//ul/li[1]/a")
+    private WebElement asideSpecialSitemapLink;
+    @FindBy(xpath = "//div[@class='menu-dialog menu-special']//ul/li[2]/a")
+    private WebElement asideSpecialProductsMapLink;
+    //news
+    @FindBy(xpath = "//div[@class='menu-dialog menu-news']//h2")
+    private WebElement asideNewsSectionTitle;
+    @FindBy(xpath = "//div[@class='news']")
+    private WebElement asideNewsArticleData;
+    @FindBy(xpath = "//div[@class='news']/a[1]")
+    private WebElement asideAllNewsLink;
+    @FindBy(xpath = "//div[@class='news']/a[2]")
+    private WebElement asideSubscribeLink;
+    //footer
+    @FindBy(xpath = "//div[@class='footer-links']//a[1]")
+    private WebElement footerHelpZoneLink;
+    @FindBy(xpath = "//div[@class='footer-links']//a[2]")
+    private WebElement footerContactUsLink;
+    @FindBy(xpath = "//div[@class='footer-links']//a[3]")
+    private WebElement footerOurSiteLink;
+    @FindBy(xpath = "//div[@class='footer-links']//a[4]")
+    private WebElement footerTermsConditionsLink;
+    @FindBy(xpath = "//div[@class='footer-links']//a[5]")
+    private WebElement footerPrivacyLink;
+    @FindBy(xpath = "//div[@class='footer-links']//a[6]")
+    private WebElement footerPrivacyStatementLink;
+    @FindBy(xpath = "//div[@class='prnotice']")
+    private WebElement footerPoweredByText;
+    @FindBy(xpath = "//div[@class='prnotice']/a")
+    private WebElement footerShoppingCartSoftwareLink;
+    @FindBy(xpath = "//div[@class='copyright']")
+    private WebElement footerCopyrightText;
+    @FindBy(xpath = "//ul[@class='soc-footer-links']/li[1]/a")
+    private WebElement footerFacebookIconButton;
+    @FindBy(xpath = "//ul[@class='soc-footer-links']/li[2]/a")
+    private WebElement footerXIconButton;
 
     public GeneralPage(WebDriver driver) {super(driver);}
 
+    //click header 'Register' link method
+    public void clickHeaderRegisterLink(){ headerRegisterLink.click();}
 
-    
+    //general page text element getters
+    public String getHeaderInterfaceTag(){return headerInterfaceTag.getText();}
+    public String getHeaderVisualSchemeTag(){return headerVisualSchemeTag.getText();}
+    public String getHeaderConfigurationTag(){return headerConfigurationTag.getText();}
+    public String getHeaderQuickAddonsPanelTag(){return headerQuickAddonsPanelTag.getText();}
+    public String getAsideCategoriesSectionTitle(){return asideCategoriesSectionTitle.getText();}
+    public String getAsideBestsellersSectionTitle(){return asideBestsellersSectionTitle.getText();}
+    public String getAsideManufacturersSectionTitle(){return asideManufacturersSectionTitle.getText();}
+    public String getAsideSpecialSectionTitle(){return asideSpecialSectionTitle.getText();}
+    public String getFooterPoweredByText(){return footerPoweredByText.getText();}
+    public String getFooterCopyrightText(){return footerCopyrightText.getText();}
+
+    //general page web element assert methods
+    //header (switcher)
+    public boolean isHeaderSwitcherIconDisplayed() {return headerSwitcherIcon.isDisplayed();}
+    public boolean isHeaderInterfaceTagDisplayed() {return headerInterfaceTag.isDisplayed();}
+    public boolean isHeaderInterfaceDropdownMenuDisplayed() {return headerInterfaceDropdownMenu.isDisplayed();}
+    public boolean isHeaderVisualSchemeTagDisplayed() {return headerVisualSchemeTag.isDisplayed();}
+    public boolean isHeaderVisualSchemeDropdownMenuDisplayed() {return headerVisualSchemeDropdownMenu.isDisplayed();}
+    public boolean isHeaderConfigurationTagDisplayed() {return headerConfigurationTag.isDisplayed();}
+    public boolean isHeaderConfigurationDropdownMenuDisplayed() {return headerConfigurationDropdownMenu.isDisplayed();}
+    public boolean isHeaderQuickAddonsPanelTagDisplayed() {return headerQuickAddonsPanelTag.isDisplayed();}
+    public boolean isHeaderAddonsPanelSlideButtonDisplayed() {return headerQuickAddonsPanelSlideButton.isDisplayed();}
+    public boolean isHeaderSwitcherDownloadButtonDisplayed() {return headerSwitcherDownloadButton.isDisplayed();}
+    //header
+    public boolean isHeaderHomepageLogoDisplayed() {return headerHomepageLogo.isDisplayed();}
+    public boolean isHeaderSignInLinkDisplayed() {return headerSignInLink.isDisplayed();}
+    public boolean isHeaderRegisterLinkDisplayed() {return headerRegisterLink.isDisplayed();}
+    public boolean isHeaderLanguageDropdownMenuDisplayed() {return headerLanguageDropdownMenu.isDisplayed();}
+    public boolean isHeaderSearchBarInputFieldDisplayed() {return headerSearchBarInputField.isDisplayed();}
+    public boolean isHeaderSearchBarButtonDisplayed() {return headerSearchBarButton.isDisplayed();}
+    public boolean isHeaderShoppingCartDropdownButtonDisplayed() {return headerShoppingCartDropdownButton.isDisplayed();}
+    //navbar
+    public boolean isNavbarHomeLinkDisplayed() {return navbarHomeLink.isDisplayed();}
+    public boolean isNavbarShoppingCartLinkDisplayed() {return navbarShoppingCartLink.isDisplayed();}
+    public boolean isNavbarContactUsLinkDisplayed() {return navbarContactUsLink.isDisplayed();}
+    //aside
+    //categories
+    public boolean isAsideCategoriesSectionTitleDisplayed() {return asideCategoriesSectionTitle.isDisplayed();}
+    public boolean isAsideFeaturesDemoLinkDisplayed() {return asideFeaturesDemoLink.isDisplayed();}
+    public boolean isAsideApparelLinkDisplayed() {return asideApparelLink.isDisplayed();}
+    public boolean isAsideToysLinkDisplayed() {return asideToysLink.isDisplayed();}
+    public boolean isAsideIGoodsLinkDisplayed() {return asideIGoodsLink.isDisplayed();}
+    public boolean isAsideBooksLinkDisplayed() {return asideBooksLink.isDisplayed();}
+    public boolean isAsideComputerHardwareLinkDisplayed() {return asideComputerHardwareLink.isDisplayed();}
+    //manufacturers category
+    public boolean isAsideManufacturersSectionTitleDisplayed() {return asideManufacturersSectionTitle.isDisplayed();}
+    //gift certificate
+    public boolean isAsideGiftCertificateButtonDisplayed() {return asideGiftCertificateButton.isDisplayed();}
+    //special
+    public boolean isAsideSpecialSectionTitleDisplayed() {return asideSpecialSectionTitle.isDisplayed();}
+    public boolean isAsideSpecialSitemapLinkDisplayed() {return asideSpecialSitemapLink.isDisplayed();}
+    public boolean isAsideSpecialProductsMapLinkDisplayed() {return asideSpecialProductsMapLink.isDisplayed();}
+    //news
+    public boolean isAsideNewsSectionTitleDisplayed() {return asideNewsSectionTitle.isDisplayed();}
+    public boolean isAsideNewsArticleDataDisplayed() {return asideNewsArticleData.isDisplayed();}
+    public boolean isAsideAllNewsLinkDisplayed() {return asideAllNewsLink.isDisplayed();}
+    public boolean isAsideSubscribeLinkDisplayed() {return asideSubscribeLink.isDisplayed();}
+    //footer
+    public boolean isFooterHelpZoneLinkDisplayed() {return footerHelpZoneLink.isDisplayed();}
+    public boolean isFooterContactUsLinkDisplayed() {return footerContactUsLink.isDisplayed();}
+    public boolean isFooterOurSiteLinkDisplayed() {return footerOurSiteLink.isDisplayed();}
+    public boolean isFooterTermsConditionsLinkDisplayed() {return footerTermsConditionsLink.isDisplayed();}
+    public boolean isFooterPrivacyLinkDisplayed() {return footerPrivacyLink.isDisplayed();}
+    public boolean isFooterPrivacyStatementLinkDisplayed() {return footerPrivacyStatementLink.isDisplayed();}
+    public boolean isFooterPoweredByTextDisplayed() {return footerPoweredByText.isDisplayed();}
+    public boolean isFooterShoppingCartSoftwareLinkDisplayed() {return footerShoppingCartSoftwareLink.isDisplayed();}
+    public boolean isFooterCopyrightTextDisplayed() {return footerCopyrightText.isDisplayed();}
+    public boolean isFooterFacebookIconButtonDisplayed() {return footerFacebookIconButton.isDisplayed();}
+    public boolean isFooterXIconButtonDisplayed() {return footerXIconButton.isDisplayed();}
+    //bestsellers section
+    public boolean isAsideBestsellersSectionTitleDisplayed() {return asideBestsellersSectionTitle.isDisplayed();}
+    //list elements
+    public boolean isAsideBestProductImageDisplayed() {
+        return asideBestProductImageElements.stream()
+                .allMatch(WebElement::isDisplayed);
+    }
+    public boolean isAsideBestProductNameLinkDisplayed() {
+        return asideBestProductNameLinkElements.stream()
+                .allMatch(WebElement::isDisplayed);
+    }
+    public boolean isAsideBestProductUnitPriceDisplayed() {
+        return asideBestProductUnitPriceElements.stream()
+                .allMatch(WebElement::isDisplayed);
+    }
+    public boolean isAsideManufacturersLinkDisplayed() {
+        return asideManufacturerLinkElements.stream()
+                .allMatch(WebElement::isDisplayed);
+    }
+
+    //these elements have different locators (added div element)
+    public boolean isOtherAsideFeaturesDemoLinkDisplayed() {return asideOtherFeaturesDemoLink.isDisplayed();}
+    public boolean isOtherAsideApparelLinkDisplayed() {return asideOtherApparelLink.isDisplayed();}
+    public boolean isOtherAsideToysLinkDisplayed() {return asideOtherToysLink.isDisplayed();}
+    public boolean isOtherAsideIGoodsLinkDisplayed() {return asideOtherIGoodsLink.isDisplayed();}
+    public boolean isOtherAsideBooksLinkDisplayed() {return asideOtherBooksLink.isDisplayed();}
+    public boolean isOtherAsideComputerHardwareLinkDisplayed() {return asideOtherComputerHardwareLink.isDisplayed();}
+
 }
 
--- src/test/java/com/xcart/demo/utilities/TestMethods.java
+++ src/test/java/com/xcart/demo/utilities/TestMethods.java
@@ -11,7 +11,158 @@ public class TestMethods extends BaseTest{
 
     protected static final Logger logger = LoggerFactory.getLogger(TestMethods.class);
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    //general page web element assert test method (aside section)
+    protected void isGeneralPageAsideWebElementDisplayed(GeneralPage generalPage){
+        //aside (categories)
+        //assert aside categories section title is displayed
+        assertTrue(generalPage.isAsideCategoriesSectionTitleDisplayed(), "The aside categories section title isn't displayed");
+        //assert aside features demo link is displayed
+        assertTrue(generalPage.isAsideFeaturesDemoLinkDisplayed(), "The aside features demo link isn't displayed");
+        //assert aside apparel link is displayed
+        assertTrue(generalPage.isAsideApparelLinkDisplayed(), "The aside apparel link isn't displayed");
+        //assert aside toys link is displayed
+        assertTrue(generalPage.isAsideToysLinkDisplayed(), "The aside toys link isn't displayed");
+        //assert aside 'I Goods' link is displayed
+        assertTrue(generalPage.isAsideIGoodsLinkDisplayed(), "The aside 'I Goods' link isn't displayed");
+        //assert aside books link is displayed
+        assertTrue(generalPage.isAsideBooksLinkDisplayed(), "The aside books link isn't displayed");
+        //assert aside computer hardware is displayed
+        assertTrue(generalPage.isAsideComputerHardwareLinkDisplayed(), "The aside computer hardware isn't displayed");
+        //manufacturers section
+        //assert aside manufacturers section title is displayed
+        assertTrue(generalPage.isAsideManufacturersSectionTitleDisplayed(), "The aside manufacturers section title isn't displayed");
+        //assert aside manufacturers section links are displayed (as a list)
+        assertTrue(generalPage.isAsideManufacturersLinkDisplayed(), "The aside manufacturers section links aren't displayed");
+        //gift certificate
+        //assert aside gift certificate button is displayed
+        assertTrue(generalPage.isAsideGiftCertificateButtonDisplayed(), "The aside gift certificate button isn't displayed");
+        //special section
+        //assert aside special section title is displayed
+        assertTrue(generalPage.isAsideSpecialSectionTitleDisplayed(), "The aside special section title isn't displayed");
+        //assert aside special sitemap link is displayed
+        assertTrue(generalPage.isAsideSpecialSitemapLinkDisplayed(), "The aside special sitemap link isn't displayed");
+        //assert aside special products map link is displayed
+        assertTrue(generalPage.isAsideSpecialProductsMapLinkDisplayed(), "The aside special products map link isn't displayed");
+        //news section
+        //assert aside news section title is displayed
+        assertTrue(generalPage.isAsideNewsSectionTitleDisplayed(), "The aside news section title isn't displayed");
+        //assert aside news article data is displayed
+        assertTrue(generalPage.isAsideNewsArticleDataDisplayed(), "The aside news article data isn't displayed");
+        //assert aside news 'all news' link is displayed
+        assertTrue(generalPage.isAsideAllNewsLinkDisplayed(), "The aside news 'all news' link isn't displayed");
+        //assert aside news subscribe link is displayed
+        assertTrue(generalPage.isAsideSubscribeLinkDisplayed(), "The aside news subscribe link isn't displayed");
+    }
+
+    //general page web element assert test method (aside section) (register page has an additional div element in aside categories list)
+    protected void isGeneralOtherPageAsideWebElementDisplayed(GeneralPage generalPage){
+        //aside (categories)
+        //assert aside categories section title is displayed
+        assertTrue(generalPage.isAsideCategoriesSectionTitleDisplayed(), "The aside categories section title isn't displayed");
+        //assert aside features demo link is displayed
+        assertTrue(generalPage.isOtherAsideFeaturesDemoLinkDisplayed(), "The aside features demo link isn't displayed");
+        //assert aside apparel link is displayed
+        assertTrue(generalPage.isOtherAsideApparelLinkDisplayed(), "The aside apparel link isn't displayed");
+        //assert aside toys link is displayed
+        assertTrue(generalPage.isOtherAsideToysLinkDisplayed(), "The aside toys link isn't displayed");
+        //assert aside 'I Goods' link is displayed
+        assertTrue(generalPage.isOtherAsideIGoodsLinkDisplayed(), "The aside 'I Goods' link isn't displayed");
+        //assert aside books link is displayed
+        assertTrue(generalPage.isOtherAsideBooksLinkDisplayed(), "The aside books link isn't displayed");
+        //assert aside computer hardware is displayed
+        assertTrue(generalPage.isOtherAsideComputerHardwareLinkDisplayed(), "The aside computer hardware isn't displayed");
+        //manufacturers section
+        //assert aside manufacturers section title is displayed
+        assertTrue(generalPage.isAsideManufacturersSectionTitleDisplayed(), "The aside manufacturers section title isn't displayed");
+        //assert aside manufacturers section links are displayed (as a list)
+        assertTrue(generalPage.isAsideManufacturersLinkDisplayed(), "The aside manufacturers section links aren't displayed");
+        //gift certificate
+        //assert aside gift certificate button is displayed
+        assertTrue(generalPage.isAsideGiftCertificateButtonDisplayed(), "The aside gift certificate button isn't displayed");
+        //special section
+        //assert aside special section title is displayed
+        assertTrue(generalPage.isAsideSpecialSectionTitleDisplayed(), "The aside special section title isn't displayed");
+        //assert aside special sitemap link is displayed
+        assertTrue(generalPage.isAsideSpecialSitemapLinkDisplayed(), "The aside special sitemap link isn't displayed");
+        //assert aside special products map link is displayed
+        assertTrue(generalPage.isAsideSpecialProductsMapLinkDisplayed(), "The aside special products map link isn't displayed");
+        //news section
+        //assert aside news section title is displayed
+        assertTrue(generalPage.isAsideNewsSectionTitleDisplayed(), "The aside news section title isn't displayed");
+        //assert aside news article data is displayed
+        assertTrue(generalPage.isAsideNewsArticleDataDisplayed(), "The aside news article data isn't displayed");
+        //assert aside news 'all news' link is displayed
+        assertTrue(generalPage.isAsideAllNewsLinkDisplayed(), "The aside news 'all news' link isn't displayed");
+        //assert aside news subscribe link is displayed
+        assertTrue(generalPage.isAsideSubscribeLinkDisplayed(), "The aside news subscribe link isn't displayed");
+        //footer
+        //assert footer 'help zone' link is displayed
+        assertTrue(generalPage.isFooterHelpZoneLinkDisplayed(), "The footer 'help zone' link isn't displayed");
+        //assert footer 'contact us' link is displayed
+        assertTrue(generalPage.isFooterContactUsLinkDisplayed(), "The footer 'contact us' link isn't displayed");
+        //assert footer 'our site' link is displayed
+        assertTrue(generalPage.isFooterOurSiteLinkDisplayed(), "The footer 'our site' link isn't displayed");
+        //assert footer 'Terms and Conditions' link is displayed
+        assertTrue(generalPage.isFooterTermsConditionsLinkDisplayed(), "The footer 'Terms and Conditions' link isn't displayed");
+        //assert footer 'privacy' link is displayed
+        assertTrue(generalPage.isFooterPrivacyLinkDisplayed(), "The footer 'privacy' link isn't displayed");
+        //assert footer 'privacy statement' link is displayed
+        assertTrue(generalPage.isFooterPrivacyStatementLinkDisplayed(), "The footer 'privacy statement' link isn't displayed");
+        //assert footer 'powered by' text is displayed
+        assertTrue(generalPage.isFooterPoweredByTextDisplayed(), "The footer 'powered by' text isn't displayed");
+        //assert footer copyright text is displayed
+        assertTrue(generalPage.isFooterCopyrightTextDisplayed(), "The footer copyright text isn't displayed");
+        //assert footer facebook icon button is displayed
+        assertTrue(generalPage.isFooterFacebookIconButtonDisplayed(), "The footer facebook icon button isn't displayed");
+        //assert footer x icon button is displayed
+        assertTrue(generalPage.isFooterXIconButtonDisplayed(), "The footer x icon button isn't displayed");
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //general page text element assert method
+    protected void isGeneralPageTextElementAsExpected(GeneralPage generalPage){
+        //header (switcher)
+        //assert header interface tag is as expected
+        assertEquals("Interface", generalPage.getHeaderInterfaceTag(), "The header interface tag doesn't match expectations.");
+        //assert header visual scheme tag is as expected
+        assertEquals("Visual Scheme", generalPage.getHeaderVisualSchemeTag(), "The header visual scheme tag doesn't match expectations.");
+        //assert header configuration tag is as expected
+        assertEquals("Configuration", generalPage.getHeaderConfigurationTag(), "The header configuration tag doesn't match expectations.");
+        //assert header quick addons panel tag is as expected
+        assertEquals("Quick addons panel", generalPage.getHeaderQuickAddonsPanelTag(), "The header quick addons panel tag doesn't match expectations.");
+        //aside
+        //assert aside categories section title is as expected
+        assertEquals("Categories", generalPage.getAsideCategoriesSectionTitle(), "The aside categories section title doesn't match expectations.");
+        //assert aside manufacturers section title is as expected
+        assertEquals("Manufacturers", generalPage.getAsideManufacturersSectionTitle(), "The aside manufacturers section title doesn't match expectations.");
+        //assert aside special section title is as expected
+        assertEquals("Special", generalPage.getAsideSpecialSectionTitle(), "The aside special section title doesn't match expectations.");
+        //assert footer powered by text is as expected
+        assertEquals("Powered by X-Cart shopping cart software", generalPage.getFooterPoweredByText(), "The footer powered by text doesn't match expectations.");
+        //assert footer copyright text is as expected
+        assertEquals("Copyright © 2002-2025 Your Company Name", generalPage.getFooterCopyrightText(), "The footer copyright text doesn't match expectations.");
+    }
+
+    //general page text element assert method (without aside section)
+    protected void isGeneralPageNoAsideTextElementAsExpected(GeneralPage generalPage){
+        //header (switcher)
+        //assert header interface tag is as expected
+        assertEquals("Interface", generalPage.getHeaderInterfaceTag(), "The header interface tag doesn't match expectations.");
+        //assert header visual scheme tag is as expected
+        assertEquals("Visual Scheme", generalPage.getHeaderVisualSchemeTag(), "The header visual scheme tag doesn't match expectations.");
+        //assert header configuration tag is as expected
+        assertEquals("Configuration", generalPage.getHeaderConfigurationTag(), "The header configuration tag doesn't match expectations.");
+        //assert header quick addons panel tag is as expected
+        assertEquals("Quick addons panel", generalPage.getHeaderQuickAddonsPanelTag(), "The header quick addons panel tag doesn't match expectations.");
+        //footer
+        //assert footer powered by text is as expected
+        assertEquals("Powered by X-Cart shopping cart software", generalPage.getFooterPoweredByText(), "The footer powered by text doesn't match expectations.");
+        //assert footer copyright text is as expected
+        assertEquals("Copyright © 2002-2025 Your Company Name", generalPage.getFooterCopyrightText(), "The footer copyright text doesn't match expectations.");
+    }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
