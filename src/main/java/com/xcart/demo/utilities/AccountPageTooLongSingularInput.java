d929ab93dcef766b4c4a456c4c7e38ede60a8b43
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 08:03:30 +0300

    Add empty AccountPageTooLongSingularInput sheet

---
+++ src/main/java/com/xcart/demo/utilities/AccountPageTooLongSingularInput.java
@@ -0,0 +1,12 @@
+package com.xcart.demo.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class AccountPageTooLongSingularInput extends BasePage{
+
+
+    public AccountPageTooLongSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
