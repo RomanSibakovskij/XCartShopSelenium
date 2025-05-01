cfe32c9c0bb9b14bd01b06ea0a15f63e9f04a2ea
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 08:09:44 +0300

    Add Invalid Edit User Account Data Test - Too Long First Name test method

--- src/main/java/com/xcart/demo/utilities/AccountPageTooLongSingularInput.java
+++ src/main/java/com/xcart/demo/utilities/AccountPageTooLongSingularInput.java
@@ -17,7 +17,52 @@ public class AccountPageTooLongSingularInput extends BasePage{
     @FindBy(xpath = "//div[@role='dialog']")
     private WebElement accountEditPageSingularInputErrorBox;
 
+    //valid edited user data (for remaining inputs)
+    private String validEditedFirstName;
+    private String validEditedLastName;
+    private String validEditedEmail;
+
+    //invalid singular edited user input data - too long singular input
+    private String tooLongEditedFirstName;
+
     public AccountPageTooLongSingularInput(WebDriver driver) {super(driver);}
+
+    //invalid edited user input data getter - too long user edited first name (100 chars)
+    public void invalidEditedUserDataTooLongFirstNameGetter() {
+
+        tooLongEditedFirstName = "Rsfdgfdgffgvfdsfaghresrfeghtyjtrewaertuyikyujthygrfsdsffghnbvcsdsfdghfggswfregrthgfsadedfhgdsfsedrhf";
+        validEditedLastName = TestDataGenerator.getRandomEditedLastName();
+        validEditedEmail = TestDataGenerator.generateRandomEmailAddress(12);
+
+        System.out.println("Generated invalid edited user input data (too long edited first name): " + "\n");
+
+        logger.info("Too long edited user first name: " + tooLongEditedFirstName);
+        logger.info("Valid edited user last name (too long edited first name): " + validEditedLastName);
+        logger.info("Valid edited user email (too long edited first name): " + validEditedEmail);
+
+        System.out.println("\n");
+    }
+
+    //invalid edited user account data input methods - too short singular input
+    public void inputTooLongEditedFirstNameIntoFirstNameInputField(){
+        accountDetailsPageFirstNameInputField.clear();
+        accountDetailsPageFirstNameInputField.sendKeys(tooLongEditedFirstName);
+    }
+
+    //valid edited user account data input methods (for remaining inputs)
+    public void inputValidEditedFirstNameIntoFirstNameInputField(){
+        accountDetailsPageFirstNameInputField.clear();
+        accountDetailsPageFirstNameInputField.sendKeys(validEditedFirstName);
+    }
+    public void inputValidEditedLastNameIntoLastNameInputField(){
+        accountDetailsPageLastNameInputField.clear();
+        accountDetailsPageLastNameInputField.sendKeys(validEditedLastName);
+    }
+    public void inputValidEditedEmailIntoEmailInputField(){
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", accountDetailsPageEmailInputField);
+        accountDetailsPageEmailInputField.clear();
+        accountDetailsPageEmailInputField.sendKeys(validEditedEmail);
+    }
 
     //singular input error box text getter
     public String getSingularInputErrorBoxMessage() {
