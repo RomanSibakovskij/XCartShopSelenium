97234c7a0a7614562cf81b08407d1ec61651f795
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 04:56:01 +0300

    Add empty RegisterPage sheet

---
+++ src/main/java/com/xcart/demo/RegisterPage.java
@@ -0,0 +1,14 @@
+package com.xcart.demo;
+
+import com.xcart.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+
+public class RegisterPage extends BasePage{
+
+
+    public RegisterPage(WebDriver driver) {super(driver);}
+
+
+}
