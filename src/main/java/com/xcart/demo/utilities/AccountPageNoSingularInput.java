5527a4eea9a6fa149fe4b9d01b31bdab32cf17bb
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 07:29:11 +0300

    Add relevant web elements for AccountPageNoSingularInput utility sheet

--- src/main/java/com/xcart/demo/utilities/AccountPageNoSingularInput.java
+++ src/main/java/com/xcart/demo/utilities/AccountPageNoSingularInput.java
@@ -5,8 +5,24 @@ import org.openqa.selenium.support.*;
 
 public class AccountPageNoSingularInput extends BasePage{
 
+    //personal information section
+    @FindBy(xpath = "//table[@class='data-table register-table']/tbody/tr[2]//input")
+    private WebElement accountDetailsPageFirstNameInputField;
+    @FindBy(xpath = "//table[@class='data-table register-table']/tbody/tr[3]//input")
+    private WebElement accountDetailsPageLastNameInputField;
+    //account information section
+    @FindBy(xpath = "//table[@class='data-table register-table']/tbody/tr[8]//input")
+    private WebElement accountDetailsPageEmailInputField;
+    //singular input error box element
+    @FindBy(xpath = "//div[@role='dialog']")
+    private WebElement accountEditPageSingularInputErrorBox;
 
     public AccountPageNoSingularInput(WebDriver driver) {super(driver);}
 
+    //singular input error box text getter
+    public String getSingularInputErrorBoxMessage() {
+        String boxErrorText = accountEditPageSingularInputErrorBox.getText();
+        return boxErrorText.replace("Close", "").replace("OK", "").trim();
+    }
 
 }
