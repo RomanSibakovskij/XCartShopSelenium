64c73428f96fafaca29a40875b5112e48b697862
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 05:53:37 +0300

    Add empty RegisterPageTooLongSingularInput utility sheet

---
+++ src/main/java/com/xcart/demo/utilities/RegisterPageTooLongSingularInput.java
@@ -0,0 +1,12 @@
+package com.xcart.demo.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class RegisterPageTooLongSingularInput extends BasePage{
+
+
+    public RegisterPageTooLongSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
