fd12207f33e250931c09e0717a612a68a1083f5d
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 06:57:32 +0300

    Add empty AccountDetailsPage sheet

---
+++ src/main/java/com/xcart/demo/AccountDetailsPage.java
@@ -0,0 +1,17 @@
+package com.xcart.demo;
+
+import com.xcart.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class AccountDetailsPage extends BasePage {
+
+
+    public AccountDetailsPage(WebDriver driver) {super(driver);}
+
+    
+}
