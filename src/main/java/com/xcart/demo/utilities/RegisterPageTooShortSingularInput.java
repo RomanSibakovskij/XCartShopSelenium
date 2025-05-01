1d5ee4ec9ac9b333be02cfa76cfed695f1eb8ea7
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 05:31:08 +0300

    Add empty RegisterPageTooShortSingularInput utility sheet

---
+++ src/main/java/com/xcart/demo/utilities/RegisterPageTooShortSingularInput.java
@@ -0,0 +1,13 @@
+package com.xcart.demo.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class RegisterPageTooShortSingularInput extends BasePage{
+
+
+
+    public RegisterPageTooShortSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
