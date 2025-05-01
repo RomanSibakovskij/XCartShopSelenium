89350423ce4801371ca6351c806dd88e06e4919c
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 07:44:46 +0300

    Add empty AccountPageTooShortSingularInput utility sheet

---
+++ src/main/java/com/xcart/demo/utilities/AccountPageTooShortSingularInput.java
@@ -0,0 +1,12 @@
+package com.xcart.demo.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class AccountPageTooShortSingularInput extends BasePage{
+
+
+    public AccountPageTooShortSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
