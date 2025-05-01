ed8904a591b3953f214281041cc1747a6e8427d4
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 06:31:55 +0300

    Add empty AccountPage sheet

---
+++ src/main/java/com/xcart/demo/AccountPage.java
@@ -0,0 +1,15 @@
+package com.xcart.demo;
+
+import com.xcart.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+import java.util.*;
+
+public class AccountPage extends BasePage{
+
+
+    public AccountPage(WebDriver driver) {super(driver);}
+
+    
+}
