5fb43422be4fcf9ad4de457eb069adc473201a7d
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 06:14:05 +0300

    Add empty RegisterPageInvalidSingularInputFormat utility sheet

---
+++ src/main/java/com/xcart/demo/utilities/RegisterPageInvalidSingularInputFormat.java
@@ -0,0 +1,13 @@
+package com.xcart.demo.utilities;
+
+import com.xcart.demo.RegisterPage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class RegisterPageInvalidSingularInputFormat extends BasePage{
+
+
+    public RegisterPageInvalidSingularInputFormat(WebDriver driver) {super(driver);}
+
+    
+}
