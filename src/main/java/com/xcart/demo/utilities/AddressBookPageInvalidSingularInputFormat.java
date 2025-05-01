7ec558b929e6f0090596571e721204bdea933fd7
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 08:21:10 +0300

    Add empty AddressBookPageInvalidSingularInputFormat utility sheet

---
+++ src/main/java/com/xcart/demo/utilities/AddressBookPageInvalidSingularInputFormat.java
@@ -0,0 +1,12 @@
+package com.xcart.demo.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class AddressBookPageInvalidSingularInputFormat extends BasePage{
+
+
+    public AddressBookPageInvalidSingularInputFormat(WebDriver driver) {super(driver);}
+
+
+}
