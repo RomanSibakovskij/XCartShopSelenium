6df63ba055d44d361b74361aff8463d4948ac0e6
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 05:14:02 +0300

    Add relevant web elements for RegisterPageNoSingularInput utility sheet

--- src/main/java/com/xcart/demo/utilities/RegisterPageNoSingularInput.java
+++ src/main/java/com/xcart/demo/utilities/RegisterPageNoSingularInput.java
@@ -5,8 +5,28 @@ import org.openqa.selenium.support.*;
 
 public class RegisterPageNoSingularInput extends BasePage{
 
+    //personal information section
+    @FindBy(xpath = "//table[@class='data-table register-table']/tbody/tr[2]//input")
+    private WebElement registerPageFirstNameInputField;
+    @FindBy(xpath = "//table[@class='data-table register-table']/tbody/tr[3]//input")
+    private WebElement registerPageLastNameInputField;
+    //account information
+    @FindBy(xpath = "//table[@class='data-table register-table']/tbody/tr[8]//input")
+    private WebElement registerPageEmailInputField;
+    @FindBy(xpath = "//table[@class='data-table register-table']/tbody/tr[10]//input[@id='passwd1']")
+    private WebElement registerPagePasswordInputField;
+    @FindBy(xpath = "//table[@class='data-table register-table']/tbody/tr[11]//input[@id='passwd2']")
+    private WebElement registerPageConfirmPasswordInputField;
+    //singular input error box element
+    @FindBy(xpath = "//div[@role='dialog']")
+    private WebElement registerPageSingularInputErrorBox;
 
     public RegisterPageNoSingularInput(WebDriver driver) {super(driver);}
 
-    
+    //singular input error box text getter
+    public String getSingularInputErrorBoxMessage() {
+        String boxErrorText = registerPageSingularInputErrorBox.getText();
+        return boxErrorText.replace("Close", "").replace("OK", "").trim();
+    }
+
 }
