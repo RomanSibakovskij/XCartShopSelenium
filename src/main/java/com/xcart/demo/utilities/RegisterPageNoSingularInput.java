062f9e06febe4dd57c54f41e744aba91eeae9615
RomanSibakovskij <benutzer373@gmail.com>
2025-05-01 05:12:38 +0300

    Add empty RegisterPageNoSingularInput utility sheet

---
+++ src/main/java/com/xcart/demo/utilities/RegisterPageNoSingularInput.java
@@ -0,0 +1,12 @@
+package com.xcart.demo.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class RegisterPageNoSingularInput extends BasePage{
+
+
+    public RegisterPageNoSingularInput(WebDriver driver) {super(driver);}
+
+    
+}
