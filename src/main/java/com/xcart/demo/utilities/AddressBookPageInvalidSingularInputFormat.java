2506dbbff358b04d5750dac001b087820f63fa65
RomanSibakovskij <benutzer373@gmail.com>
2025-05-02 07:16:58 +0300

    Add relevant web elements for AddressBookPageInvalidSingularInputFormat utility sheet

--- src/main/java/com/xcart/demo/utilities/AddressBookPageInvalidSingularInputFormat.java
+++ src/main/java/com/xcart/demo/utilities/AddressBookPageInvalidSingularInputFormat.java
@@ -5,9 +5,27 @@ import org.openqa.selenium.support.*;
 
 public class AddressBookPageInvalidSingularInputFormat extends BasePage{
 
-
+    //new address form elements
+    @FindBy(xpath = "//input[@id='firstname']")
+    private WebElement newAddressFormFirstNameInputField;
+    @FindBy(xpath = "//input[@id='lastname']")
+    private WebElement newAddressFormLastNameInputField;
+    @FindBy(xpath = "//input[@id='address']")
+    private WebElement newAddressFormAddress1InputField;
+    @FindBy(xpath = "//input[@id='city']")
+    private WebElement newAddressFormCityInputField;
+    @FindBy(xpath = "//input[@id='zipcode']")
+    private WebElement newAddressFormPostCodeInputField;
+    //singular input error box element
+    @FindBy(xpath = "//div[@role='dialog'][2]")
+    private WebElement addressBookPageSingularInputErrorBox;
 
     public AddressBookPageInvalidSingularInputFormat(WebDriver driver) {super(driver);}
 
+    //singular input error box text getter
+    public String getSingularInputErrorBoxMessage() {
+        String boxErrorText = addressBookPageSingularInputErrorBox.getText();
+        return boxErrorText.replace("Close", "").replace("OK", "").trim();
+    }
 
 }
