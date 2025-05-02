a4e15b88912325b0e1c2eea3b3d7ec1e163287ab
RomanSibakovskij <benutzer373@gmail.com>
2025-05-02 04:56:52 +0300

    Add relevant web elements for AccountPageInvalidSingularInputFormat utility sheet

--- src/main/java/com/xcart/demo/utilities/AccountPageInvalidSingularInputFormat.java
+++ src/main/java/com/xcart/demo/utilities/AccountPageInvalidSingularInputFormat.java
@@ -5,7 +5,24 @@ import org.openqa.selenium.support.*;
 
 public class AccountPageInvalidSingularInputFormat extends BasePage{
 
+    //personal information section
+    @FindBy(xpath = "//table[@class='data-table register-table']/tbody/tr[2]//input")
+    private WebElement accountDetailsPageFirstNameInputField;
+    @FindBy(xpath = "//table[@class='data-table register-table']/tbody/tr[3]//input")
+    private WebElement accountDetailsPageLastNameInputField;
+    //account information section
+    @FindBy(xpath = "//table[@class='data-table register-table']/tbody/tr[8]//input")
+    private WebElement accountDetailsPageEmailInputField;
+    //singular input error box element
+    @FindBy(xpath = "//div[@role='dialog']")
+    private WebElement accountEditPageSingularInputErrorBox;
 
     public AccountPageInvalidSingularInputFormat(WebDriver driver) {super(driver);}
-    
+
+    //singular input error box text getter
+    public String getSingularInputErrorBoxMessage() {
+        String boxErrorText = accountEditPageSingularInputErrorBox.getText();
+        return boxErrorText.replace("Close", "").replace("OK", "").trim();
+    }
+
 }
