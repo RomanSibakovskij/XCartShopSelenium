7be1343d3ad891d69f3c81cb19f1a6020b087381
RomanSibakovskij <benutzer373@gmail.com>
2025-05-02 08:11:33 +0300

    Add empty SignInPageInvalidInputScenarios utility sheet

---
+++ src/main/java/com/xcart/demo/utilities/SignInPageInvalidInputScenarios.java
@@ -0,0 +1,13 @@
+package com.xcart.demo.utilities;
+
+import com.xcart.demo.RegisterPage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class SignInPageInvalidInputScenarios extends BasePage{
+
+
+    public SignInPageInvalidInputScenarios(WebDriver driver) {super(driver);}
+
+    
+}
