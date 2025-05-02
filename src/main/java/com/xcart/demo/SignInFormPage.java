6fad4327a58be2eea3dcdda8e584609413289808
RomanSibakovskij <benutzer373@gmail.com>
2025-05-02 07:41:55 +0300

    Add empty SignInFormPage sheet

---
+++ src/main/java/com/xcart/demo/SignInFormPage.java
@@ -0,0 +1,14 @@
+package com.xcart.demo;
+
+import com.xcart.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+
+public class SignInFormPage extends BasePage{
+
+
+    public SignInFormPage(WebDriver driver) {super(driver);}
+
+    
+}
