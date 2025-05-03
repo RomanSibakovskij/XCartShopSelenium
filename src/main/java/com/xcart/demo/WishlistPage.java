ae347a16ccbb76a7ccc1a844ee893ab45e2b76b3
RomanSibakovskij <benutzer373@gmail.com>
2025-05-03 06:50:19 +0300

    Add empty WishlistPage sheet

---
+++ src/main/java/com/xcart/demo/WishlistPage.java
@@ -0,0 +1,19 @@
+package com.xcart.demo;
+
+import com.xcart.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.util.*;
+import java.time.Duration;
+import java.util.stream.Collectors;
+
+public class WishlistPage extends BasePage {
+
+
+    public WishlistPage(WebDriver driver) {super(driver);}
+
+
+}
