707561fff3ca61b8f3e10db4849c4a7c1fe7428c
RomanSibakovskij <benutzer373@gmail.com>
2025-05-03 06:15:43 +0300

    Add Single Product Addition From Single Product Page To Cart Test (as a registered user) method

--- src/test/java/com/xcart/demo/SingleProductPageTest.java
+++ src/test/java/com/xcart/demo/SingleProductPageTest.java
@@ -18,5 +18,22 @@ public class SingleProductPageTest extends TestMethods{
         addSingleDigitalAngelProductToCartGuestTest();
     }
 
+    //Test 009a -> add single product ("Digital Angel") from single product page to cart test (as a registered user)
+    @Test
+    @DisplayName("Single Product Addition From Single Product Page To Cart Test (as a registered user)")
+    @Tag("Add_Single_Product_To_Cart")
+    @Tag("Single_Product_Page_Test")
+    @Tag("Test_As_A_Reg_User")
+    void singleProductPageProductRegUserAddToCartTest() {
+        RegisterPage registerPage = new RegisterPage(driver);
+        //navigate user to 'Register' page test
+        navigateToRegisterPageTest();
+        //valid user account creation test
+        validUserAccountCreationTest(registerPage);
+        //valid user address addition test
+        validUserAddressAdditionTest();
+        //add single product ("Digital Angel") from single product page to cart test (as a registered user)
+        addSingleDigitalAngelProductToCartRegUserTest();
+    }
 
 }
--- src/test/java/com/xcart/demo/utilities/TestMethods.java
+++ src/test/java/com/xcart/demo/utilities/TestMethods.java
@@ -3913,6 +3913,58 @@ protected void navigateToRegisterPageTest(){
         //capture screenshot of the test result
         captureScreenshot(driver, "Add Single Product ('Digital Angel') From Single Product Page To Cart Test Result (guest)");
     }
+    //add single product ('Digital Angel') from single product page to cart test method (as a registered user)
+    protected void addSingleDigitalAngelProductToCartRegUserTest(){
+        GeneralPage generalPage = new GeneralPage(driver);
+        HomePage homePage = new HomePage(driver);
+        AccountPage accountPage = new AccountPage(driver);
+        SingleProductPage singleProductPage = new SingleProductPage(driver);
+        //general page web element assert (without aside section)
+        isGeneralPageNoAsideWebElementDisplayed(generalPage);
+        //general page aside section web element assert (this page has an additional div in aside categories section)
+        isGeneralOtherPageAsideWebElementDisplayed(generalPage);
+        //general page text element assert
+        isGeneralPageTextElementAsExpected(generalPage);
+        //additional general page web element assert
+        isAddGeneralPageWebElementDisplayed(generalPage);
+        //additional general page text element assert
+        isAddGeneralPageTextElementAsExpected(generalPage);
+        //click 'Home' navbar link
+        generalPage.clickNavbarHomeLink();
+        //home page web element assert
+        isHomePageWebElementDisplayed(homePage);
+        //home page text element assert
+        isHomePageTextElementAsExpected(homePage);
+        //log home page product data
+        logHomePageProductData(homePage);
+        //click 'Digital Angel' product name link
+        homePage.clickFeaturedProductNameLink(1);
+        //capture screenshot of the set single product page
+        captureScreenshot(driver, "Single Product ('Digital Angel') Page Display");
+        //assert the user got onto correct product page
+        assertEquals("Digital Angel [Product Options Demo]", singleProductPage.getSingleProductPageTitle(), "The single product page title doesn't match expectations or the user is on the wrong product page.");
+        //single product page web element assert
+        isSingleProductPageWebElementDisplayed(singleProductPage);
+        //log single product page data
+        logSingleProductPageProductData(singleProductPage);
+        //click 'Customers also bought; link
+        singleProductPage.clickCustomersAlsoBoughtLink();
+        //single product page ('Digital Angel') web element assert
+        isSingleProductTShirtPageWebElementDisplayed(singleProductPage);
+        //log specific single product page data
+        logSingleProductTShirtPageProductData(singleProductPage);
+        logSingleProductTShirtPageCustomersBoughtProductData(singleProductPage);
+        //click size dropdown menu
+        singleProductPage.clickProductSizeDropdownMenu();
+        //select 'L' size
+        singleProductPage.selectLSizeOption();
+        //click 'Add to cart' button
+        singleProductPage.clickAddToCartButton();
+        //click shopping cart form 'View Cart' button (since the shopping cart form elements somehow are not being seen as in other pages, separate click method is being used)
+        singleProductPage.clickViewCartShoppingCartFormButton();
+        //capture screenshot of the test result
+        captureScreenshot(driver, "Add Single Product ('Digital Angel') From Single Product Page To Cart Test Result (registered user)");
+    }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
