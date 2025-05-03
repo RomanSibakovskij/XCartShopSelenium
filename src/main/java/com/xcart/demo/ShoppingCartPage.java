da369fd7ce6be7c6b5d4338e18db70b9e8efced5
RomanSibakovskij <benutzer373@gmail.com>
2025-05-03 07:19:57 +0300

    Add empty ShoppingCartPage sheet

---
+++ src/main/java/com/xcart/demo/ShoppingCartPage.java
@@ -0,0 +1,19 @@
+package com.xcart.demo;
+
+import com.xcart.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ShoppingCartPage extends BasePage {
+
+
+    public ShoppingCartPage(WebDriver driver) {super(driver);}
+
+    
+}
