8e33b00aa190c2591b8e2daf8a470472f7ef3756
RomanSibakovskij <benutzer373@gmail.com>
2025-05-03 05:08:45 +0300

    Add empty SingleProductCategoryDashboardPage sheet

---
+++ src/main/java/com/xcart/demo/SingleProductCategoryDashboardPage.java
@@ -0,0 +1,18 @@
+package com.xcart.demo;
+
+import com.xcart.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SingleProductCategoryDashboardPage extends BasePage{
+
+
+    public SingleProductCategoryDashboardPage(WebDriver driver) {super(driver);}
+
+    
+}
