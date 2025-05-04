bb31b96a356c7d251e46108edbb9244df8ab8abb
RomanSibakovskij <benutzer373@gmail.com>
2025-05-04 05:13:16 +0300

    Add empty OrderInvoicePage sheet

---
+++ src/main/java/com/xcart/demo/OrderInvoicePage.java
@@ -0,0 +1,18 @@
+package com.xcart.demo;
+
+import com.xcart.demo.utilities.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class OrderInvoicePage extends BasePage {
+
+
+    public OrderInvoicePage(WebDriver driver) {super(driver);}
+
+
+    
+}
