eeac01f105d29fbb7c254bf01e2db5d5a416a0a8
RomanSibakovskij <benutzer373@gmail.com>
2025-05-04 05:07:48 +0300

    Add empty CheckoutPage sheet

---
+++ src/main/java/com/xcart/demo/CheckoutPage.java
@@ -0,0 +1,18 @@
+package com.xcart.demo;
+
+import com.xcart.demo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class CheckoutPage extends BasePage{
+
+
+    public CheckoutPage(WebDriver driver) {super(driver);}
+
+
+    
+}
