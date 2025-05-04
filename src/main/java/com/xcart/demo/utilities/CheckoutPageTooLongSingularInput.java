64d8c79f1057b3bf79458da2772f1669ff7c5d92
RomanSibakovskij <benutzer373@gmail.com>
2025-05-04 07:14:03 +0300

    Add empty CheckoutPageTooLongSingularInput utility sheet

---
+++ src/main/java/com/xcart/demo/utilities/CheckoutPageTooLongSingularInput.java
@@ -0,0 +1,12 @@
+package com.xcart.demo.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class CheckoutPageTooLongSingularInput extends BasePage{
+
+
+    public CheckoutPageTooLongSingularInput(WebDriver driver) {super(driver);}
+
+
+}
