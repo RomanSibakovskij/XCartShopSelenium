57f82448b88d62c42f9e67418dde41340b5d7201
RomanSibakovskij <benutzer373@gmail.com>
2025-05-05 05:09:55 +0300

    Add empty CheckoutPageInvalidSingularInputFormat utility sheet

---
+++ src/main/java/com/xcart/demo/utilities/CheckoutPageInvalidSingularInputFormat.java
@@ -0,0 +1,12 @@
+package com.xcart.demo.utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
+public class CheckoutPageInvalidSingularInputFormat extends BasePage{
+
+
+    public CheckoutPageInvalidSingularInputFormat(WebDriver driver) {super(driver);}
+
+    
+}
